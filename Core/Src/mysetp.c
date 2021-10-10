f6d12f15e673763d8fccae220430fb28e850e97c
毛小文 <neoway@126.com>
2021-10-10 18:08:33 +0800

    init

--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -1,14 +1,21 @@
 #include "mysetp.h"
 
 
+u8g2_t u8g2;
 
-void lvgl_setp(){
-
-}
 
 
 void setp(){
-    lvgl_setp();
+    HAL_Delay(1000);
+    u8g2Init(&u8g2);
+    HAL_Delay(1000);
+    while (1){
+        u8g2_ClearBuffer(&u8g2);
+        u8g2_SetFont(&u8g2, u8g2_font_10x20_tr);
+        u8g2_DrawBitmap
+        u8g2_SendBuffer(&u8g2);
+
+    }
 }
 
 
