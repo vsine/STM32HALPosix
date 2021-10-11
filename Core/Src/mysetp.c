b9bde9586e2a0ae6a8cce2f0050f448f9e6c9ae3
毛小文 <neoway@126.com>
2021-10-11 17:03:10 +0800

    init

--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -6,7 +6,7 @@ u8g2_t u8g2;
 FATFS fs;
 FATFS *pfs;
 FIL fil;
-
+//FRESULT fr; 
 
 UINT br;
 BYTE frame[1024];
@@ -20,13 +20,17 @@ void setp(){
     while (1){
     u8g2_ClearBuffer(&u8g2);   
     f_read(&fil,frame,1024,&br);
+    if(br==0){
+        f_close(&fil);
+        f_open(&fil, "0:/badapple.txt", FA_READ);
+    }
     for (int k = 0; k < 8; k++)
         for (int x = 0; x < 128; x++)
             for (int y = 0; y < 8; y++)
                 if(frame[k*128+x]&(0x01<<y))
                     u8g2_DrawPixel(&u8g2,x,y+(k*8));
     u8g2_SendBuffer(&u8g2);
-    HAL_Delay(1);
+
     }
 }
 
