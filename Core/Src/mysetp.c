4f82c5bd4097e97839fdc9ae4d67cf2bff28c68a
毛小文 <neoway@126.com>
2021-10-25 20:01:17 +0800

    init

--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -11,15 +11,19 @@ void setp(){
     u8g2_DrawStr(&u8g2,10,55,"X(K|K):0.1");
     //u8g2_DrawLine(&u8g2,20,20,40,20);
     u8g2_SendBuffer(&u8g2);
-    while (1)
-    {
-       
-    }
+
     
 }
 
 void loop(){
 
+    u8g2_ClearBuffer(&u8g2);
+    u8g2_SetFont(&u8g2,u8g2_font_courB12_tf);
+    u8g2_DrawStr(&u8g2,10,20,"fs:ok");
+    u8g2_DrawStr(&u8g2,10,40,"dfs:ok");
+    u8g2_DrawStr(&u8g2,10,55,"X(K|K):0.1");
+    //u8g2_DrawLine(&u8g2,20,20,40,20);
+    u8g2_SendBuffer(&u8g2);
 }
 
 
