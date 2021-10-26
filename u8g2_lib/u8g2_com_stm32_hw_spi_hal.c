ffc8db83b044444cd1782101c851c869c506fd8a
毛小文 <neoway@126.com>
2021-10-26 21:46:35 +0800

    init

--- u8g2_lib/u8g2_com_stm32_hw_spi_hal.c
+++ u8g2_lib/u8g2_com_stm32_hw_spi_hal.c
@@ -49,8 +49,10 @@ uint8_t u8x8_stm32_gpio_and_delay(U8X8_UNUSED u8x8_t *u8x8,U8X8_UNUSED uint8_t m
 
 void u8g2Init(u8g2_t *u8g2)
 {
+    //四线spi
     u8g2_Setup_ssd1306_128x64_noname_f(u8g2, U8G2_R0, u8x8_byte_4wire_hw_spi, u8x8_stm32_gpio_and_delay); // 初始化 u8g2 结构体
     u8g2_InitDisplay(u8g2);                                                                       // 根据所选的芯片进行初始化工作，初始化完成后，显示器处于关闭状态
     u8g2_SetPowerSave(u8g2, 0);                                                                   // 打开显示器
     //u8g2_ClearBuffer(u8g2);
+   
 }
