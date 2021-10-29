22b88de797cf2dedd65efa92f124e60200a04291
毛小文 <neoway@126.com>
2021-10-29 20:23:06 +0800

    init

--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -2,10 +2,15 @@
 #include "stdlib.h"
 
 
-
+u8g2_t u8g2;
 
 void setp(){
-
+    HAL_Delay(100);
+    u8g2Init(&u8g2);
+    u8g2_ClearBuffer(&u8g2);
+    u8g2_SetFont(&u8g2,u8g2_font_courB12_tf);
+    u8g2_DrawStr(&u8g2,10,20,"test");
+    u8g2_SendBuffer(&u8g2);
 
 }
 
