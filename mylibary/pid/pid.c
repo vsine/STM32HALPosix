1d231d10523ef1a52b057849c9a27ef97bb14b9c
毛小文 <neoway@126.com>
2021-10-29 19:58:18 +0800

    init

--- mylibary/pid/pid.c
+++ mylibary/pid/pid.c
@@ -29,46 +29,28 @@ float PIDController_Update(PIDController *pid, float setpoint, float measurement
 	* Proportional
 	*/
     float proportional = pid->Kp * error;
-
-
 	/*
 	* Integral
 	*/
     pid->integrator = pid->integrator +  pid->Ki *   error ;
 
 	/* Anti-wind-up via integrator clamping */
-    if (pid->integrator > pid->limMaxInt) {
-
+    if (pid->integrator > pid->limMaxInt)
         pid->integrator = pid->limMaxInt;
-
-    } else if (pid->integrator < pid->limMinInt) {
-
+    else if (pid->integrator < pid->limMinInt)
         pid->integrator = pid->limMinInt;
-
-    }
-
-
 	/*
 	* Derivative (band-limited differentiator)
 	*/
-		
     pid->differentiator = pid->Kd*((error-pid->prevError));
-
-
 	/*
 	* Compute output and apply limits
 	*/
     pid->out = proportional + pid->integrator + pid->differentiator;
-
-    if (pid->out > pid->limMax) {
-
+    if (pid->out > pid->limMax)
         pid->out = pid->limMax;
-
-    } else if (pid->out < pid->limMin) {
-
+    else if (pid->out < pid->limMin)
         pid->out = pid->limMin;
-
-    }
 
 	/* Store error and measurement for later use */
     pid->prevError       = error;
