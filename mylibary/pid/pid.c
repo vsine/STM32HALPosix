57891d23cc715d7fd42ea17a3e111cf51157f5f7
毛小文 <neoway@126.com>
2021-10-29 20:09:39 +0800

    init

--- mylibary/pid/pid.c
+++ mylibary/pid/pid.c
@@ -1,30 +1,23 @@
 #include "pid.h"
 void PIDController_Init(PIDController *pid) {
-
 	/* Clear controller variables */
 	pid->integrator = 0.0f;
 	pid->prevError  = 0.0f;
-
 	pid->differentiator  = 0.0f;
 	pid->prevMeasurement = 0.0f;
-
 	pid->out = 0.0f;
-
 }
 
 float PIDController_Update(PIDController *pid, float setpoint, float measurement) {
-
 	/*
 	* Error signal
 	*/
     float error = setpoint - measurement;
-
 	/*
 	* State switch
 	*/
 	if (error*pid->prevError<0)
 		pid->prevError=0;
-	
 	/*
 	* Proportional
 	*/
@@ -33,7 +26,6 @@ float PIDController_Update(PIDController *pid, float setpoint, float measurement
 	* Integral
 	*/
     pid->integrator = pid->integrator +  pid->Ki *   error ;
-
 	/* Anti-wind-up via integrator clamping */
     if (pid->integrator > pid->limMaxInt)
         pid->integrator = pid->limMaxInt;
@@ -51,11 +43,9 @@ float PIDController_Update(PIDController *pid, float setpoint, float measurement
         pid->out = pid->limMax;
     else if (pid->out < pid->limMin)
         pid->out = pid->limMin;
-
 	/* Store error and measurement for later use */
     pid->prevError       = error;
     pid->prevMeasurement = measurement;
-
 	/* Return controller output */
     return pid->out;
 }
