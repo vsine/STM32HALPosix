1c51ce7f40c139d8a8e48189aa6d21f16e734070
毛小文 <neoway@126.com>
2021-10-30 13:04:31 +0800

    init

--- Core/Src/mysetp.c
+++ Core/Src/mysetp.c
@@ -18,7 +18,7 @@ void loop(){
 
     u8g2_ClearBuffer(&u8g2);
     u8g2_SetFont(&u8g2,u8g2_font_courB12_tf);
-    u8g2_DrawStr(&u8g2,10,30,"DDF");
+    u8g2_DrawStr(&u8g2,10,30,"eXe");
     u8g2_SendBuffer(&u8g2);
     HAL_Delay(1);
 
