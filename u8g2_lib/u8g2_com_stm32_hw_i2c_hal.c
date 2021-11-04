0f0ea3aff338a6e1093abb2b773db0abab1032ea
毛小文 <neoway@126.com>
2021-11-04 23:00:24 +0800

    init

--- u8g2_lib/u8g2_com_stm32_hw_i2c_hal.c
+++ u8g2_lib/u8g2_com_stm32_hw_i2c_hal.c
@@ -81,7 +81,40 @@ uint8_t u8x8_gpio_and_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *ar
     }
     return 1;
 }
+uint8_t u8x8_byte_stm32_hw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+	/* u8g2/u8x8 will never send more than 32 bytes between START_TRANSFER and END_TRANSFER */
+	static uint8_t buffer[32];
+	static uint8_t buf_idx;
+	uint8_t *data;
 
+	switch(msg)
+	{
+	case U8X8_MSG_BYTE_SEND:
+		data = (uint8_t *)arg_ptr;
+		while( arg_int > 0 )
+		{
+			buffer[buf_idx++] = *data;
+			data++;
+			arg_int--;
+		}
+		break;
+	case U8X8_MSG_BYTE_INIT:
+		/* add your custom code to init i2c subsystem */
+		break;
+	case U8X8_MSG_BYTE_SET_DC:
+		break;
+	case U8X8_MSG_BYTE_START_TRANSFER:
+		buf_idx = 0;
+		break;
+	case U8X8_MSG_BYTE_END_TRANSFER:
+		if(HAL_I2C_Master_Transmit(&hi2c1,OLED_ADDRESS, buffer, buf_idx, 1000) != HAL_OK) return 0;
+		break;
+	default:
+		return 0;
+	}
+	return 1;
+}
 void u8g2Init(u8g2_t *u8g2)
 {
     u8g2_Setup_ssd1306_i2c_128x64_noname_f(u8g2, U8G2_R0, u8x8_byte_hw_i2c, u8x8_gpio_and_delay); // 初始化 u8g2 结构体
